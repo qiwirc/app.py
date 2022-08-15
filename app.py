dcd3ad66818dfdd7d1e2ca7ee9e5725ec4a1679c
qiwirc <110579762+qiwirc@users.noreply.github.com>
2022-08-15 20:48:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,43 +6,52 @@ from kivy.uix.textinput import TextInput
 
 class MainApp(App):
     def build(self):
-        main_layout = BoxLayout(orientation="vertical", padding=10, spacing=10)
-        self.solution = TextInput(multiline=False, readonly=False, halign="right", font_size=55)
+        main_layout = BoxLayout(orientation="vertical", padding=10, spacing=15)
+        self.solution = TextInput(multiline=False, readonly=False, halign="center", font_size=130)
+        self.colution = TextInput(multiline=False, readonly=False, halign="center", font_size=130)
+        self.holution = TextInput(multiline=False, readonly=True, halign="center", font_size=100)
+        self.holution1 = TextInput(multiline=False, readonly=True, halign="center", font_size=100, text = 'Введите свой рост:')
+        self.holution2 = TextInput(multiline=False, readonly=True, halign="center", font_size=100, text = 'Введите свой вес:')
+        self.holution3 = TextInput(multiline=False, readonly=True, halign="center", font_size=100, text = 'Имт вашего тела:')
+        main_layout.add_widget(self.holution1)
         main_layout.add_widget(self.solution)
+        main_layout.add_widget(self.holution2)
+        main_layout.add_widget(self.colution)
+        main_layout.add_widget(self.holution3)
+        main_layout.add_widget(self.holution)
         buttons = [
-            ["7", "8", "9", "/"],
-            ["4", "5", "6", "*"],
-            ["1", "2", "3", "-"],
-            [".", "0", "C", "+"],
+        ['Очистить']
         ]
         for row in buttons:
             h_layout = BoxLayout()
             for label in row:
-                button = Button(text=label, pos_hint={"center_x": 0.5, "center_y": 0.5})
+                button = Button(text=label, pos_hint={"center_x": 0.5, "center_y": 0.5}, font_size=100)
                 button.bind(on_press=self.on_button_press)
                 h_layout.add_widget(button)
             main_layout.add_widget(h_layout)
 
-        equals_button = Button(text="=", pos_hint={"center_x": 0.5, "center_y": 0.5})
+        equals_button = Button(text="Рассчитать ИМТ", pos_hint={"center_x": 0.5, "center_y": 0.5}, font_size=100)
         equals_button.bind(on_press=self.on_solution)
         main_layout.add_widget(equals_button)
+       
 
         # return a Button() as a root widget
         return main_layout
+        
 
     def on_button_press(self, instance):
-        if instance.text == "C":
+        if instance.text == "Очистить":
             self.solution.text = ""
+            self.colution.text = ""
+            self.holution.text = ""
         else:
             self.solution.text += instance.text
 
     def on_solution(self, instance):
-        if self.solution.text:
-            try:
-                self.solution.text = str(eval(self.solution.text))
-            except:
-                self.solution.text = "Error"
-
-
+        a = int(self.solution.text)
+        b = int(self.colution.text)
+        self.holution.text = str(b/((a/100)*(a/100)))
+                
+                
 if __name__ == '__main__':
     MainApp().run()
